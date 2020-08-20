e8fc34e6777e613b9e2033ad0b1e7da7658b2a13
ono shinji <yjsnpi555@gmail.com>
2020-08-20 17:51:32 +0900

    ランダム文字列で画像保存しないようにした

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,17 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :fog
+
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # storage :fog
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process :resize_to_limit => [800,600]
-
   version :user_thumb do
     process resize_to_fit: [40, 40]
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,23 +2,23 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
+  # 変更箇所。うまく動作しない場合は、下記のif文を消す
+  if Rails.env.production? # 本番環境:AWS
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
       path_style:            true,
-  }
-  config.fog_public     = true # 直リンクでのアクセスを許可する
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'} # キャッシュ保存する日時
-  case Rails.env
-    when 'production'
-      config.fog_directory = 'teachers-production'
-      config.asset_host = 'https://teachers-production.s3.amazonaws.com'
-    when 'development'
-      config.fog_directory = 'teachers-development'
-      config.asset_host = 'https://teachers-development.s3.amazonaws.com'
+    }
+    config.fog_public     = true # 直リンクでのアクセスを許可する
+    config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'} # キャッシュ保存する日時
+    config.fog_directory = 'teachers-production'
+    config.asset_host = 'https://teachers-production.s3.amazonaws.com'
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
   end
 end
