8b3072536ae9eafd046f08457a30d631cd3855ae
ono shinji <yjsnpi555@gmail.com>
2020-08-26 14:55:17 +0900

    エラーメッセージの表示をdevelop環境で変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # }
   config.cache_classes = false
   config.eager_load = false
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
   config.x.webpacker[:dev_server_host] = "http://localhost:8080"
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
