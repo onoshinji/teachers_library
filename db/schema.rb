48ed7e04b63e5515ea40ad7eefb5b3999935f1a6
ono shinji <yjsnpi555@gmail.com>
2020-10-22 22:31:01 +0900

    画像プレビュー機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_141536) do
+ActiveRecord::Schema.define(version: 2020_10_19_123249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
