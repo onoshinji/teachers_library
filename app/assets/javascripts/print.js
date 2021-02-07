514b57d3f9fe59d3a8a8e12806f548872ba592ac
ono shinji <yjsnpi555@gmail.com>
2021-02-07 16:20:21 +0900

    印刷ボタンが正しく機能するように修正

--- app/assets/javascripts/print.js
+++ app/assets/javascripts/print.js
@@ -1,18 +1,13 @@
-$(function(){
-//印刷ボタンをクリックした時の処理
-$('.print-secondary').on('click', function(){
+function funcprint(){
 
-//プリントしたいエリアの取得
-var printArea = document.getElementsByClassName("print-area");
+  var printArea = document.getElementsByClassName("print-area");
+  $('body').append('<div id="print" class="printBc"></div>');
+  $(printArea).clone().appendTo('#print');
 
-//プリント用の要素「#print」を作成し、上で取得したprintAreaをその子要素に入れる。
-$('body').append('<div id="print" class="printBc"></div>');
-$(printArea).clone().appendTo('#print');
-
-//この下に、以降の処理が入ります。
-$('body > :not(#print)').addClass('print-off');
-window.print();
-$('#print').remove();
-$('.print-off').removeClass('print-off');
- });
-});
+  //プリントしたいエリア意外に、非表示のcssが付与されたclassを追加
+  $('body > :not(#print)').addClass('print-off');
+  window.print();
+  //window.print()を実行した後、作成した「#print」と、非表示用のclass「print-off」を削除
+  $('#print').remove();
+  $('.print-off').removeClass('print-off');
+};
